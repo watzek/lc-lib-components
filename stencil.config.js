a1d0e67a0c436aaf517ef7e223e1dac5ac7aaa93
Nicholas Budak <budak@c993122.local>
2018-02-06 22:15:20 -0800

    turn off generateWWW

--- stencil.config.js
+++ stencil.config.js
@@ -1,6 +1,7 @@
 exports.config = {
   namespace: 'lc-lib-components',
   generateDistribution: true,
+  generateWWW: false,
   bundles: [
     { components: ['library-header', 'library-info', 'library-search'] },
   ]
